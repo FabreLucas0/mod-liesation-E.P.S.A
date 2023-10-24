094ca0583bb245ddbca940a524acb07f25828f09
Yanis_TRIGLIA <Yannis.Triglia@alithya.com>
2023-10-24 22:01:28 +0200

    Ajout d'un exemple de graphiqe animée

--- Evolution_age.py
+++ Evolution_age.py
@@ -50,29 +50,34 @@ def temp (Mat,Vec):
 print(temp(matrice,vector))
 #%%
 
+#graphique intéressant pour adapter à l'évolution du vecteur a des temps différent
+
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from matplotlib import rc
+from matplotlib import pyplot as plt
+from matplotlib import animation
 
-fig = plt.figure(1)
-plt.axis([0,2*np.pi,-1,1])
+# First set up the figure, the axis, and the plot element we want to animate
+fig = plt.figure()
+ax = plt.axes(xlim=(0, 2), ylim=(0, 10))
+line, = ax.plot([], [], lw=2)
 
-N = 40                     
+# initialization function: plot the background of each frame
+def init():
+    line.set_data([], [])
+    return line,
 
-x = np.linspace(0,2*np.pi,100)  
+# animation function.  This is called sequentially
+def animate(i):
+    x = np.linspace(0, 2, 1000)
+    y = np.sin(2 * np.pi * (x - 0.01 * i))
+    line.set_data(x, y)
+    return line,
 
-liste_images = []           
-
-for i in range(N):
-    im = plt.plot(x,np.sin(x-i*(2*np.pi)/N),'b')
-    liste_images.append(im)
-
-
-
-anim = animation.ArtistAnimation(fig, liste_images, interval=150, repeat_delay=1000)
-
+# call the animator.  blit=True means only re-draw the parts that have changed.
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=200, interval=20, blit=True)
 
 
-rc('animation', html='jshtml')
-anim
+anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+
+plt.show()
