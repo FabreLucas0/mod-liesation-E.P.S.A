a74314da6cc244b04ba6759b13e996d70596463a
Yanis-TRIGLIA <yanis.triglia@etu.univ-amu.fr>
2023-12-01 10:49:05 +0100

    Ajout graphique animée

--- Evolution_age.py
+++ Evolution_age.py
@@ -8,13 +8,26 @@ Created on Wed Oct 18 14:09:41 2023
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
-#%%
-matrice = np.array([[0.6,0.35,0.5,1],[0.4,0,0,0],[0,0.65,0,0],[0,0,0.5,0]])
+#on défini notre matrice et notre vecteur
+matrice = np.array([[0.6,0.35,0.5,1],
+                    [0.4,0,0,0],
+                    [0,0.65,0,0],
+                    [0,0,0.5,0]])
+
+
+
+matrice2 = [[0.73, 1.25, 1.25, 1.25, 0],
+             [0.39, 0, 0, 0, 0],
+             [0, 0.67, 0, 0, 0],
+             [0, 0, 0.67, 0, 0],
+             [0, 0, 0, 0.64, 0]]
 
 vector = np.array([10,0,0,0])
 
-def temp (Mat,Vec):
+
+def grap_without_animation(Mat,Vec):
     
     t = Mat
     v = Vec
@@ -25,59 +38,71 @@ def temp (Mat,Vec):
     a3 =[v[3]]
     
     for i in range(30):
-        
+        #on multiplie la matrice par le vecteur pour obtenir la multiplication de la matrice fois le vecteur
         vect = np.dot(t,v)
-        print(vect)
         v = vect
         y = 10
+        #chaque a correspondent respectivement à l'indice du vecteur obtenur
         a0.append(v[0])
         a1.append(v[1])
         a2.append(v[2])
         a3.append(v[3])
         
-     
         plt.plot(a0,'red')
         plt.plot(a1,'green')
         plt.plot(a2,'blue')
         plt.plot(a3,'yellow')
-        
-        
+         
         plt.plot(y)
         plt.show()      
         
-   
 
-print(temp(matrice,vector))
-#%%
+def graph_with_animation1(matrice,vector):
 
-#graphique intéressant pour adapter à l'évolution du vecteur a des temps différent
+    #on défini notre graphe ou chaque ligne correspond à un vecteur pour l'instant vide
+    fig, ax = plt.subplots()
+    ligne, = ax.plot([], [], 'o-', label='a0')
+    ligne1, = ax.plot([], [], 'v-', label='a1')
+    ligne2, = ax.plot([], [], '-', label='a2')
+    ligne3, = ax.plot([], [], '-', label='a3')
+    
 
-import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib import animation
+    def init():
+        #on limite notre graphe
+        ax.set_xlim(0, 30)
+        ax.set_ylim(0, 12)
+        ax.legend()
+        return ligne, ligne1, ligne2, ligne3
 
-# First set up the figure, the axis, and the plot element we want to animate
-fig = plt.figure()
-ax = plt.axes(xlim=(0, 2), ylim=(0, 10))
-line, = ax.plot([], [], lw=2)
+    def update(frame):
+        global vector
+        #On va multiplier la matrice par le cecteur tant que update et appeler
+        vect = np.dot(matrice, vector)
+        vector = vect
+        a0.append(vect[0])
+        a1.append(vect[1])
+        a2.append(vect[2])
+        a3.append(vect[3])
+        #on met a jour els ligne qui coresspondent respectivement a a0,a1,a2,a3
+        ligne.set_data(range(frame + 2), a0[:frame + 2])
+        ligne1.set_data(range(frame + 2), a1[:frame + 2])
+        ligne2.set_data(range(frame + 2), a2[:frame + 2])
+        ligne3.set_data(range(frame + 2), a3[:frame + 2])
+        return ligne, ligne1, ligne2, ligne3
 
-# initialization function: plot the background of each frame
-def init():
-    line.set_data([], [])
-    return line,
-
-# animation function.  This is called sequentially
-def animate(i):
-    x = np.linspace(0, 2, 1000)
-    y = np.sin(2 * np.pi * (x - 0.01 * i))
-    line.set_data(x, y)
-    return line,
-
-# call the animator.  blit=True means only re-draw the parts that have changed.
-anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=200, interval=20, blit=True)
+    #on donne le point de départ de nos vecteur
+    a0 = [vector[0]]
+    a1 = [vector[1]]
+    a2 = [vector[2]]
+    a3 = [vector[3]]
 
 
-anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+    #on lance l'animation en appelant update 20 fois
+    ani = FuncAnimation(fig, update, frames=20, init_func=init, blit=True)
 
-plt.show()
+    plt.title("évolution d'une population modéle académique")
+    plt.show()
+
+graph_with_animation1(matrice,vector)
+grap_without_animation(matrice,vector)
+
